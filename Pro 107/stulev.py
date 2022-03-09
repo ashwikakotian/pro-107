8513b1370c28b2cef74a535d72046e83da94aba4
ashwikakotian <73691424+ashwikakotian@users.noreply.github.com>
2022-03-09 18:55:05 +0530

    Add files via upload

---
+++ Pro 107/stulev.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly_express as pd
+import csv 
+import plotly.graph_objects as go
+df =pd.read_csv("stulev.csv")
+
+print(df.groupby ("level")["attempt"].mean())
+fig=go.Figure(go.Bar(x=df.groupby("level")["attempt"].mean(),y=["Level 1","Level 2","Level 3","Level 4"],orientation="h"))
+fig.show()
