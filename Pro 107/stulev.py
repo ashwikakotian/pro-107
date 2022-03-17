cdd0d5fcadaa682c4766ad137dcf6c2c2ede3b89
ashwikakotian <73691424+ashwikakotian@users.noreply.github.com>
2022-03-17 17:10:47 +0530

    Update stulev.py

--- Pro 107/stulev.py
+++ Pro 107/stulev.py
@@ -5,5 +5,5 @@ import plotly.graph_objects as go
 df =pd.read_csv("stulev.csv")
 
 print(df.groupby ("level")["attempt"].mean())
-fig=go.Figure(go.Bar(x=df.groupby("level")["attempt"].mean(),y=["Level 1","Level 2","Level 3","Level 4"],orientation="h"))
+fig=go.Figure(go.Scatter(x=df.groupby("level")["attempt"].mean(),y=["Level 1","Level 2","Level 3","Level 4"],orientation="h"))
 fig.show()
